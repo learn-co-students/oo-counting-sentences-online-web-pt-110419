542d08c4c50e1e1b3bc3be76c7d77b4f008ba7bf
shahwarspall <shevy.spall@gmail.com>
2019-11-25 23:55:40 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      return true 
+    else self.end_with?("/")
+      return false
+    end
     
-  end
+  
+
+    #binding.pry
+
+
+end 
 
   def question?
-
+    if self.end_with?("?")
+      return true 
+    else self.end_with?("/")
+      return false
+    end
+    
+    # binding.pry
   end
 
   def exclamation?
+    if self.end_with?("!")
+      return true 
+    else self.end_with?("/")
+      return false
+    end
+    
 
   end
 
   def count_sentences
+    self.strip.tr(".?!", ".").split(".").reject {|x| x.empty?}.count
+    #binding.pry
 
   end
 end
