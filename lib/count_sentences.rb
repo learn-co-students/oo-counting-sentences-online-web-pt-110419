b19d2b57bb5a26645ea5052a1fa217bce9ef2fa1
Sheriefs Gamble <sgamble284@gmail.com>
2019-11-30 17:27:01 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,36 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
-    
+    if self.end_with?(".") == true
+      true
+    else
+      false
+    end
   end
-
+  
   def question?
-
+    if self.end_with?("?") == true
+      true
+    else
+      false
+    end
   end
-
+  
   def exclamation?
-
+    if self.end_with?("!") == true
+      true
+    else
+      false
+    end
   end
-
+  
   def count_sentences
-
+    arr = self.split(/\?|\.|!+|\u2026/)
+    #binding.pry
+    arr.count
+    #binding.pry
   end
+  
 end
