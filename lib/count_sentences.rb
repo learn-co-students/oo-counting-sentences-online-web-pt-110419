8794b442fca1afabce86e05bceae09d10b90d2fe
Stephen Sanchez <stephensanchez05@gmail.com>
2019-12-03 01:38:54 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+attr_accessor :array
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+     self.end_with?("?")
   end
 
   def exclamation?
-
+     self.end_with?("!")
   end
 
   def count_sentences
-
+  self.split(/\.|\?|\!/).delete_if {|s| s.size < 2}.size
   end
 end
