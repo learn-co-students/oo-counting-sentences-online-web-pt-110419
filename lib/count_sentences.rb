772e29d8eeb2c4a078a1e1c0f43ce5d8c016ea15
William Sanchez <williamsanchez.8055@gmail.com>
2019-12-11 02:28:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
+attr_accessor :sentence, :question, :exclamation, :count_sentences 
   def sentence?
-    
+   self.end_with?(".")
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
   def count_sentences
-
+    "one two three".split.count!!!!!!!!!
   end
 end
